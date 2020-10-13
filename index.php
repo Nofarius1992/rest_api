901aaf96f291016a51df4a1ed70d4b127e7c6fa4
Nofarius1992 <tolya.prozhoga@gmail.com>
2020-10-13 07:18:29 +0300

    Drew up a work plan

--- index.php
+++ index.php
@@ -3,17 +3,34 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
 	<title>REST_API</title>
 </head>
 <body>
 
 	<h1>Магазин товаров</h1>
-	<section class="card">
-		<h4 class="title_card"></h4>
-		<img src="" alt="" class="image_card">
-		<p class="description_card"></p>
-		<button>Купить</button>
-	</section>
+	<section class="products">
+        <ul>
+            <li>Orange 100$/kg</li>
+            <li>Banana 70$/kg</li>
+            <li>Apple 40$/kg</li>
+            <li>Strawberry 150$/kg</li>
+            <li>Lemon 95$/kg</li>
+        </ul>
+    </section>
+
+    <form action="add_order.php" method="POST">
+        <label>
+            <input name="email" type="email" placeholder="You email">
+        </label>
+        <label>
+            <input name="name" type="text" placeholder="Name product">
+        </label>
+        <label>
+            <input name="price" type="text" placeholder="Price USD">
+        </label>
+        <button type="submit">to Order</button>
+    </form>
 
 </body>
 </html>
